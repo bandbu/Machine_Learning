10216de2bd042e9f6aa71c8920e3eceee03769f2
bandbu <vukhoa2012@gmail.com>
2022-04-22 17:51:16 +0700

    Add files via upload

--- FaceDe.py
+++ FaceDe.py
@@ -11,6 +11,7 @@ import imutils
 import cv2
 import time
 from imutils.video import FileVideoStream
+import _thread
 
 
 vid_path='D:/test_hard.mp4'
@@ -49,51 +50,64 @@ args = vars(ap.parse_args())
 
 # start the file video stream thread and allow the buffer to
 # start to fill
+frames=[]
+minframe=30
+def render_clip(threadName):
+    global frames
+    print(threadName)
+    while fvs.more():        
+        frame = fvs.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = np.dstack([frame, frame, frame])
+        frame = imutils.resize(frame, width=450)
+        # display the size of the queue on the frame
+
+        # face_locations=fr.face_locations(frame)
+        # for loc in face_locations:
+        #     cv2.rectangle(frame, (loc[1], loc[0]), (loc[3], loc[2]), (0, 255, 0), 2)
+        
+        cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()),
+            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2) 
+        frames.append(frame)
+        print('rendering:'+str(len(frames)))
+    
+
+def showx(threadName):
+    global minframe
+    global frames
+    print(threadName)
+    if len(frames)>= minframe:
+        print("Showing clip")
+        minframe=0
+        print(len(frames))
+        for i in frames:
+            cv2.imshow("Frame", i)
+            #frames.remove(i)
+            waitKey(1)
+
 print("[INFO] starting video file thread...")
 fvs = FileVideoStream(args["video"]).start()
-time.sleep(1.0)
+count=0
 
-# start the FPS timer
-fps = FPS().start()
+      
+#length = int(fvs.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+#print(length)
+try:
+    t1=_thread.start_new_thread( render_clip, ("Thread-1",) )
+    time.sleep(30)
+    while count<600:
+        if(count <len(frames)):
+            frame=frames[count]
+            count+=1
+            cv2.imshow("video",frame)
+            waitKey(1)
+        else:
+            time.sleep(15)
+        time.sleep(0.05)
+except:
+   print("Error: unable to start thread")
 
-  
-# loop over frames from the video file stream
-count =0
-while fvs.more():
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale (while still retaining 3
-	# channels)
-    frame = fvs.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = np.dstack([frame, frame, frame])
-    frame = imutils.resize(frame, width=450)
-	# display the size of the queue on the frame
-   
-    if count==0:
-        face_locations=fr.face_locations(frame)
-    if count%5==0:
-        face_locations=fr.face_locations(frame)
-    for loc in face_locations:
-        cv2.rectangle(frame, (loc[1], loc[0]), (loc[3], loc[2]), (0, 255, 0), 2)
-    
-    cv2.putText(frame, "Queue Size: {}".format(fvs.Q.qsize()),
-		(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)    
-	# show the frame and update the FPS counter
-    cv2.imshow("Frame", frame)
-    fps.update()
-    count+=1
-    waitKey(1)
-    #k = cv2.waitKey(30) & 0xff  
-    #if k==27:  
-     #   break
-    
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-fvs.stop()
+
 
